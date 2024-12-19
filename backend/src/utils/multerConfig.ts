d4aac303169159af44c81525be0ed2f1aa939560
Havock48 <adit.github@gmail.com>
2024-12-19 21:03:37 +0100

    backend - update multer configuration

--- backend/src/utils/multerConfig.ts
+++ backend/src/utils/multerConfig.ts
@@ -1,21 +1,6 @@
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
 
-const uploadsDir = path.join(process.cwd(), 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir);
-}
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadsDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + file.originalname;
-        cb(null, uniqueSuffix);
-    }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
