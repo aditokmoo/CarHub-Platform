b4c1a4f615c9d406c58cd77fc76033cdddee7b24
Havock48 <adit.github@gmail.com>
2025-01-22 12:58:30 +0100

    frontend - service provider responsive style added

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -3,7 +3,7 @@ import styles from './Modal.module.scss';
 
 export default function Modal({ children }: { children: React.ReactNode }) {
     const portalElement = document.getElementById('portal');
-    
+
     if (!portalElement) return null;
 
     return ReactDOM.createPortal(
