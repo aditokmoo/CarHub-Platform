9794c6a1b042a691d1d583ad76e9a5bc0bfc5526
Havock48 <adit.github@gmail.com>
2025-05-06 23:41:03 +0200

    type fixed

--- client/src/features/serviceProviders/components/Providers/Providers.tsx
+++ client/src/features/serviceProviders/components/Providers/Providers.tsx
@@ -2,23 +2,31 @@ import Card from '../../../../components/Card/Card';
 import { ImFileEmpty } from 'react-icons/im';
 import { UserResponse } from '../../../auth/types';
 import styles from './Providers.module.scss';
+import { InfiniteData } from '@tanstack/react-query';
+
+interface Page {
+  users: UserResponse[];
+  currentPage: number;
+  hasMore: boolean;
+}
 
 interface PropTypes {
-  data: {
-    users: UserResponse[];
-  };
+  data?: InfiniteData<Page, unknown>;
 }
 
 export default function Providers({ data }: PropTypes) {
+  const allUsers = data?.pages.flatMap((page) => page.users) ?? [];
+
   return (
     <div className={styles.providers}>
       <div className="container">
         <div className={styles.providersLayout}>
-          {data?.users?.map((user: UserResponse) => (
-            <Card user={user} key={user.name} />
+          {allUsers.map((user: UserResponse) => (
+            <Card user={user} key={user._id} />
           ))}
         </div>
-        {data?.users?.length === 0 && (
+
+        {allUsers.length === 0 && (
           <div className={styles.noProviders}>
             <h2 className={styles.message}><ImFileEmpty /> No service providers found</h2>
           </div>
