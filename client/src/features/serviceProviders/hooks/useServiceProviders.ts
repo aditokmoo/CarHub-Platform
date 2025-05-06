a917644fa72dc846b2705db93cb226d30ce0cb31
Havock48 <adit.github@gmail.com>
2025-05-06 23:30:45 +0200

    remove unused imports

--- client/src/features/serviceProviders/hooks/useServiceProviders.ts
+++ client/src/features/serviceProviders/hooks/useServiceProviders.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getUsersBy } from "../services/serviceProviderServices";
 import { useEffect, useState } from "react";
 
